5a2531b1b285ef2b3f4e8bfca1f72051caab657e
duyiblue <duyiblue@outlook.com>
2025-10-16 18:42:01 -0700

    Updated README and example script

--- script.sh
+++ script.sh
@@ -3,6 +3,5 @@
 python alignment_training.py \
     --source_config /orion/u/duyi/cross-emb/taming-transformers/configs/latent_diffusion_ckpt_config.yaml \
     --target_config /orion/u/duyi/cross-emb/taming-transformers/configs/latent_diffusion_ckpt_config.yaml \
-    --apply_mask \
-    --corruption_severity 0 \
+    --dataset robot \
     --batch_size 12
